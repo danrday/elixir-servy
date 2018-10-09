ab4bb4e1ed99d027706fa47acdf8e4aa7252f357
euf7315 <Daniel.Day@ParallonTechnology.com>
2018-10-09 16:50:54 -0500

    servy folder

---
+++ lib/servy/handler.ex
@@ -0,0 +1,19 @@
+defmodule Servy.Handler do
+
+  request = """
+  GET /wildthings HTTP/1.1
+  Host: example.com
+  User-Agent: ExampleBrowser/1.0
+  Accept: */*
+
+"""
+
+  expected_response =   """
+  HTTP/1.1 200 OK
+  Content-Type: text/html
+  Content-Length: 20
+
+  Bears, Lions, Tigers
+  """
+
+end
