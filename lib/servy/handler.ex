e24ab3818807edc121e33c6891f3d8f38decc76e
euf7315 <Daniel.Day@ParallonTechnology.com>
2019-08-12 11:03:44 -0500

    serve static files

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -6,6 +6,7 @@ defmodule Servy.Handler do
     |> log
     |> route
     |> track
+    |> emojify
     |> format_response
   end
 
@@ -46,9 +47,42 @@ defmodule Servy.Handler do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
+  def route(%{method: "GET", path: "/bears/new"} = conv) do
+    File.read("pages/form.html")
+    |> handle_file(conv)
+  end
+
   def route(%{method: "GET", path: "/bears" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
+
+  def route(%{method: "GET", path: "/about"} = conv) do
+      File.read("pages/about.html")
+      |> handle_file(conv)
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{conv | status: 200, resp_body: content}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "File not found!"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "File Error: #{reason}"}
+  end
+
+#  def route(%{method: "GET", path: "/about"} = conv) do
+#    case File.read("pages/about.html") do
+#      {:ok, content} ->
+#        %{conv | status: 200, resp_body: content}
+#      {:error, :enoent} ->
+#        %{conv | status: 404, resp_body: "File not found!"}
+#      {:error, reason} ->
+#        %{conv | status: 500, resp_body: "File Error: #{reason}"}
+#    end
+#  end
 
   def route(%{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
@@ -63,6 +97,15 @@ defmodule Servy.Handler do
     #{conv.resp_body}
     """
   end
+
+  def emojify(%{status: 200} = conv) do
+    emojies = String.duplicate("🎉", 5)
+    body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
+
+    %{ conv | resp_body: body }
+  end
+
+  def emojify(conv), do: conv
 
   defp status_reason(code) do
     %{
@@ -116,6 +159,31 @@ IO.puts response
 
 request = """
 GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+request = """
+GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears/new HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
